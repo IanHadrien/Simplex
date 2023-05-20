ec95f7527f6677afb46e6b336cf7b0b0b08ac7cc
Ian Hadrien <ianhadrien@gmail.com>
2023-05-20 13:17:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="bg-slate-600">
+        <p className="font-bold">
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="bg-red-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
